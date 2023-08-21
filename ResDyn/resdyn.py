a9254243849ca3a2732b0f8ba62160ddf198c283
Victor Rollano <113518059+VRGQuantumTech@users.noreply.github.com>
2023-08-21 10:21:31 +0200

    ResDyn v0.1.1
    Resonator dynamics simulation (ResDyn).
    This library comprises functions to simulate the resonator behaviour solving
    the differential equations obtained from input-output theory and the master
    equation of a cavity coupled to a thermal bath.
           
    # v0.1.1 - In development.
                    Functions to simulate a naked resonator finished.
                    Developing resonator/spin dynamic functions.
                    Added some documentation for better readability.

--- ResDyn/resdyn.py
+++ ResDyn/resdyn.py
@@ -6,11 +6,13 @@ Created on Fri Jan  6 15:26:35 2023
 
 Resonator dynamics simulation (ResDyn).
 This library comprises functions to simulate the resonator behaviour solving
-the Langevin differential equations.
+the differential equations obtained from input-output theory and the master
+equation of a cavity coupled to a thermal bath.
        
-# v0.1 - In development.
-         Functions to simulate a naked resonator finished.
-         Developing resonator/spin dynamic functions.
+# v0.1.1 - In development.
+           Functions to simulate a naked resonator finished.
+           Developing resonator/spin dynamic functions.
+           Added some documentation for better readability.
         
 """
 
@@ -21,9 +23,66 @@ def wave_in(t, A):
     return 1j*A
 
 def out(t, A, wc, wd, k):
+    
+    """
+    Temporal evolution of the expectation value for the destruction operator at
+    the output port of the cavity (b_out) when a microwave signal is applied
+    (for t < t' where t' is the time at which the signal is interrumpted). This
+    equation is obtained solving the master equation for a cavity with two
+    ports in an input-output paradigm.
+    
+    Parameters
+    -------
+    t: time. Numpy array or float. In seconds.
+    A: amplitude of the input microwave signal.
+    wc: cavity resonance frequency. Float. In Hz.
+    wd: input singal frequency. Float. In Hz.
+    k: cavity losses. Float. In Hz.
+    
+    Returns
+    -------
+    b: Temporal evolution of the expectation value for the destruction operator
+       of the microwave signal at the output of the cavity.
+    
+    """
+
     d = (1j*(wc - wd) + k)
     b = 1j*k*A / d
     b = b*(np.exp(-1*d*t) - 1) + 1j*A
+    
+    return b
+
+def decay(t, t0, A, wc, wd, k):
+    
+    """
+    Temporal evolution of the expectation value for the destruction operator at
+    the output port of the cavity (b_out) when a microwave signal has been
+    applied and then interrupted and the cavity evolves after the steady regime
+    (for t > t' where t' is the time at which the signal is interrumpted).
+    This equation is obtained solving the master equation for a cavity with two
+    ports in an input-output paradigm.
+    
+    Parameters
+    -------
+    t: time. Numpy array or float. In seconds.
+    t0: time at which the input microwave signal was interrupted. Float. 
+        In seconds.
+    A: amplitude of the input microwave signal.
+    wc: cavity resonance frequency. Float. In Hz.
+    wd: input singal frequency. Float. In Hz.
+    k: cavity losses. Float. In Hz.
+    
+    Returns
+    -------
+    b: Temporal evolution of the expectation value for the destruction operator
+       of the microwave signal at the output of the cavity. There is a
+       microwave signal because the resonator is releasing energy.
+    
+    """
+    
+    d = (1j*(wc - wd) + k)
+    b = 1j*k*A / d
+    b = b * np.exp(-1*d*(t - t0))
     
     return b
 
@@ -31,17 +90,11 @@ def abs_out(t, A, wc, wd, k):
     
     return abs(out(t, A, wc, wd, k))
 
-def decay(t, t0, A, wc, wd, k):
-    d = (1j*(wc - wd) + k)
-    b = 1j*k*A / d
-    b = b * np.exp(-1*d*(t - t0))
-    
-    return b
 
 def abs_decay(t, t0, A, wc, wd, k):
     
     return abs(decay(t, t0, A, wc, wd, k))
-#%%
+#%% TEST SCRIPT
 
 t = np.linspace(0, 1e-4, 1000) # s
 tp = np.linspace(1e-4, 2e-4, 1000) # s
@@ -61,7 +114,7 @@ for i in range(len(wd)):
     data[i,len(tp):] = abs_decay(tp, t[-1], np.mean(A), wc, wd[i], k)
 
 time =  np.append(t,tp)
-plt.figure()
+plt.figure(dpi = 1200)
 plt.pcolor(time*1e6, (wd - wc)*1e-6, data, cmap = 'plasma')
 plt.tick_params(direction = 'in')
 plt.xlabel('$time$ $(\mu s)$', fontsize=14.0)
@@ -82,3 +135,4 @@ def cavsp_dynamics(state, t, wc, ws, wd, kc, ki, g, gamma,eps):
     dsm = -1j*(ws/2 - wd)*sm - 2*g*sm*a*np.exp(-1j*wd*t) - gamma*sm
     
     dsz = -1j*sz + g*(a * np.conj)"""
+
